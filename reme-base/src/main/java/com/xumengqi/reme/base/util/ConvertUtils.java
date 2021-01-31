77dedebae1fa272c8151acbb25e067d7b7095fb7
xumengqi <266347978@qq.com>
2021-01-31 18:51:08 +0800

    feature: ConvertUtils 添加 toList 方法

--- reme-base/src/main/java/com/xumengqi/reme/base/util/ConvertUtils.java
+++ reme-base/src/main/java/com/xumengqi/reme/base/util/ConvertUtils.java
@@ -2,13 +2,24 @@ package com.xumengqi.reme.base.util;
 
 import org.springframework.beans.BeanUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author xumengqi
  * @date 2021/1/31 16:45
  */
 public class ConvertUtils {
 
-    public static <T> T convertTo(Object source, Class<T> target) {
+    public static <O, T> List<T> toList(List<O> source, Class<T> target) {
+        if (source == null || source.size() == 0) {
+            return new ArrayList<>();
+        }
+        return source.stream().map(e -> toObj(e, target)).collect(Collectors.toList());
+    }
+
+    public static <T> T toObj(Object source, Class<T> target) {
         try {
             T targetInstance = target.newInstance();
             copyProperties(source, targetInstance);
