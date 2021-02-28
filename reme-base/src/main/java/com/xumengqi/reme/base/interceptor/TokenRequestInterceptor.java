c1974bf91c25b55170ad89cb434b8bf15dec46c1
xumengqi <266347978@qq.com>
2021-02-28 19:31:53 +0800

    feature: 将 userId 注入到 BaseRequest 中去

--- reme-base/src/main/java/com/xumengqi/reme/base/interceptor/TokenRequestInterceptor.java
+++ reme-base/src/main/java/com/xumengqi/reme/base/interceptor/TokenRequestInterceptor.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.xumengqi.reme.base.BaseResponse;
 import com.xumengqi.reme.base.annotations.AccessToken;
 import com.xumengqi.reme.base.conf.SystemConfig;
-import com.xumengqi.reme.base.util.JwtUtils;
 import com.xumengqi.reme.base.util.RedisUtils;
 import com.xumengqi.reme.common.enums.ErrorCodeEnum;
 import com.xumengqi.reme.common.enums.RedisKeyPrefixEnum;
@@ -19,7 +18,9 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.http.MimeHeaders;
 import java.io.PrintWriter;
+import java.lang.reflect.Field;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
@@ -50,6 +51,9 @@ public class TokenRequestInterceptor  implements HandlerInterceptor {
         boolean isHasAnnotationOnClass = handlerMethod.getBeanType().getAnnotation(AccessToken.class) != null;
         boolean isHasAnnotationOnMethod = handlerMethod.getMethod().getAnnotation(AccessToken.class) != null;
         if (!(isHasAnnotationOnClass || isHasAnnotationOnMethod)) {
+            // 安全处理
+            reflectPutHeader(request, "Authorization", null);
+            reflectPutHeader(request, "operatorCode", null);
             return true;
         }
         String token = request.getHeader("Authorization");
@@ -76,6 +80,8 @@ public class TokenRequestInterceptor  implements HandlerInterceptor {
         if (redisUtils.getExpire(key) < TimeUnit.DAYS.toSeconds(systemConfig.getAccessTokenCheckTimeInDay())) {
             redisUtils.expire(key, TimeUnit.DAYS.toSeconds(systemConfig.getAccessTokenExpireTimeInDay()));
         }
+        // 将 userId 放到 headers 中
+        reflectPutHeader(request, "operatorId", userId);
         return true;
     }
 
@@ -85,6 +91,30 @@ public class TokenRequestInterceptor  implements HandlerInterceptor {
         response.setContentType("application/json");
         try (PrintWriter writer = response.getWriter()) {
             writer.print(new Gson().toJson(baseResponse));
+        }
+    }
+
+    /**
+     * 修改 header 信息
+     * @param request /
+     * @param key /
+     * @param value /
+     */
+    private void reflectPutHeader(HttpServletRequest request, String key, String value){
+        Class<? extends HttpServletRequest> requestClass = request.getClass();
+        try {
+            Field request1 = requestClass.getDeclaredField("request");
+            request1.setAccessible(true);
+            Object o = request1.get(request);
+            Field coyoteRequest = o.getClass().getDeclaredField("coyoteRequest");
+            coyoteRequest.setAccessible(true);
+            Object o1 = coyoteRequest.get(o);
+            Field headers = o1.getClass().getDeclaredField("headers");
+            headers.setAccessible(true);
+            MimeHeaders o2 = (MimeHeaders) headers.get(o1);
+            o2.addValue(key).setString(value);
+        } catch (Exception e) {
+            log.error("修改 header 失败", e);
         }
     }
 }
