c9e85bffe666542d55b93d1883b139c4e82af7fa
bananahab <43674441+bananahab@users.noreply.github.com>
2021-03-02 22:34:40 +0800

    fix<base>: 显示基础请求内容

--- reme-base/src/main/java/com/xumengqi/reme/base/BaseResponse.java
+++ reme-base/src/main/java/com/xumengqi/reme/base/BaseResponse.java
@@ -9,13 +9,13 @@ import io.swagger.annotations.ApiModelProperty;
  * @date 2020/12/28 19:26
  */
 public class BaseResponse {
-    @ApiModelProperty(value = "错误码", hidden = true)
+    @ApiModelProperty(value = "错误码")
     private Integer errorNo;
 
-    @ApiModelProperty(value = "错误信息", hidden = true)
+    @ApiModelProperty(value = "错误信息")
     private String errorInfo;
 
-    @ApiModelProperty(value = "成功与否", hidden = true)
+    @ApiModelProperty(value = "成功与否")
     private boolean success;
 
     public void setSuccess(boolean success) {
