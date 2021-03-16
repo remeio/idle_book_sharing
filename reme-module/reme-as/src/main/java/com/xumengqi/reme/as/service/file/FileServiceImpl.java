1483ea0e57420152ddae7e3b8c94f063a959ac06
xumengqi <266347978@qq.com>
2021-03-16 16:14:43 +0800

    fix: 添加未携带 token 判断

--- reme-module/reme-as/src/main/java/com/xumengqi/reme/as/service/file/FileServiceImpl.java
+++ reme-module/reme-as/src/main/java/com/xumengqi/reme/as/service/file/FileServiceImpl.java
@@ -3,9 +3,11 @@ package com.xumengqi.reme.as.service.file;
 import com.xumengqi.reme.api.file.FileService;
 import com.xumengqi.reme.api.file.response.UploadFileResponse;
 import com.xumengqi.reme.as.logic.file.FileLogic;
+import com.xumengqi.reme.base.util.AssertUtils;
 import com.xumengqi.reme.base.util.RedisUtils;
 import com.xumengqi.reme.common.enums.ErrorCodeEnum;
 import com.xumengqi.reme.common.enums.RedisKeyPrefixEnum;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,6 +29,7 @@ public class FileServiceImpl implements FileService {
 
     @Override
     public UploadFileResponse upload(MultipartFile[] files, String token) {
+        AssertUtils.asserter().assertTrue(StringUtils.isNotBlank(token)).elseThrow(ErrorCodeEnum.NO_TOKEN);
         // 将 token 与缓存中的进行对比
         String key = RedisKeyPrefixEnum.ACCESS_TOKEN.getPrefix() + token;
         String userId = redisUtils.get(key);
